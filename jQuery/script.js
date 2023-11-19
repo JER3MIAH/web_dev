b71cdf4aca8222f8581c182a260bcb357f682d20
JER3MIAH <jeremiahbrownie@gmail.com>
2023-11-19 14:46:02 +0100

    manipulating html with jQuery

--- jQuery/script.js
+++ jQuery/script.js
@@ -1,6 +1,12 @@
+//* manipulating css styles with jQuery
 $("h1").css("color", "red");
 
+//* manipulating html with jQuery
+$("h1").html("<em>yooo</em>");
+
+//* adding classes with jQuery
 $("footer").addClass("footer bold");
 
-// manipulating text with jQuery
+//* manipulating text with jQuery
 $("button").text("Tap me");
+
