960658d18565147bbb99c55b77912c553e2cd0fa
JER3MIAH <jeremiahbrownie@gmail.com>
2023-11-19 17:29:48 +0100

    add sounds

--- Simon-Game/game.js
+++ Simon-Game/game.js
@@ -1,23 +1,34 @@
 let gamePattern = [];
 
 const buttonColours = ["red", "blue", "green", "yellow"];
+const buttonSounds = ["sounds/red.mp3", "sounds/blue.mp3", "sounds/green.mp3", "sounds/yellow.mp3"];
 
 
-let randomChosenColour = buttonColours[nextSequence()];
-gamePattern.push(randomChosenColour);
+// let randomChosenColour = buttonColours[nextSequence()];
+// gamePattern.push(randomChosenColour);
 
-function nextSequence() {
-    return Math.floor(Math.random() * 4);
-}
+// function nextSequence() {
+//     return Math.floor(Math.random() * 4);
+// }
 
-let btnTapped = $("#" + randomChosenColour);
+// let btnTapped = $("#" + randomChosenColour);
 
 $(".btn").on("click", function () {
-    let $this = $(this);
-    $this.addClass("pressed");
-    setTimeout(function () {
-        $this.removeClass("pressed");
-    }, 100);
+    onButtonClick(this);
+    audioPlay(this);
 });
 
+function onButtonClick(element) {
+    $(element).addClass("pressed");
+    setTimeout(function () {
+        $(element).removeClass("pressed");
+    }, 100);
+}
 
+function audioPlay(element) {
+    let buttonIndex = buttonColours.indexOf($(element).attr("id"));
+    if (buttonIndex) {
+        let audio = new Audio(buttonSounds[buttonIndex]);
+        audio.play();
+    }
+}
