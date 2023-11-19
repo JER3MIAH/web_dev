1f992770c4c59c2075544c2ff23a255e941ea380
JER3MIAH <jeremiahbrownie@gmail.com>
2023-11-19 15:16:16 +0100

    Adding new elements with jQuery

--- jQuery/script.js
+++ jQuery/script.js
@@ -1,19 +1,34 @@
-//* manipulating css styles with jQuery
+//* manipulating css styles
 $("h1").css("color", "red");
 
-//* manipulating html with jQuery
+//* manipulating html
 $("h1").html("<em>yooo</em>");
 
-//* adding classes with jQuery
+//* adding classes
 $("footer").addClass("footer bold");
 
-//* manipulating text with jQuery
+//* manipulating text
 $("button").text("Tap me");
 
-//* manipulating attributes with jQuery
+//* manipulating attributes
 $("a").attr("href", "https://www.yahoo.com");
 
-//* Adding event listeners with jQuery
+//* Adding event listeners
 $("button").click(function () {
     $("h1").css("color", "yellow");
 });
+
+$(document).keydown(function (event) {
+    $("h1").text(event.key);
+});
+
+$("h1").on("mouseover", function () {
+    $("h1").css("color", "black");
+});
+
+//* Adding new elements
+$("h1").before("<button>New</button>");
+$("h1").after("<button>New</button>");
+$("h1").prepend("<button>New</button>");
+$("h1").append("<button>New</button>");
+
