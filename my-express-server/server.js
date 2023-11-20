49366efe958622b588818b3a72ab914371767e2d
JER3MIAH <jeremiahbrownie@gmail.com>
2023-11-20 21:18:25 +0100

    more on express.js

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -6,6 +6,14 @@ app.get("/", function (req, res) {
     res.send("<h1>Yooooo</h1>");
 });
 
+app.get("/contact", function (req, res) {
+    res.send("Contact me at: jeremiahkenneth0@gmail.com");
+});
+
+app.get("/About", function (req, res) {
+    res.send("Heyy<br>I am Jeremiah<br>I am quite strong you know.");
+});
+
 app.listen(3000, function () {
     console.log("server started at port: 3000");
 });
