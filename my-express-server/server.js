49226a91848c534d50386043072b1ca8a25b2798
JER3MIAH <jeremiahbrownie@gmail.com>
2023-11-20 18:47:56 +0100

    server requests with express

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express(3000);
 
+app.get("/", function (req, res) {
+    res.send("<h1>Yooooo</h1>");
+});
+
 app.listen(3000, function () {
     console.log("server started at port: 3000");
 });
