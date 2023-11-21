748281af9b358cffb9c85df65789f244d8b1b345
JER3MIAH <jeremiahbrownie@gmail.com>
2023-11-21 21:03:31 +0100

    parsing json

--- weather_site/app.js
+++ weather_site/app.js
@@ -1,23 +1,35 @@
+const apiKey = require("./constants");
 const express = require("express");
 const https = require("https");
-const apiKey = require("./constants")
 
 const app = express();
 
-
-
 app.get("/", function (req, res) {
-
     const url = "https://api.openweathermap.org/data/2.5/forecast?q=london&appid=" + apiKey + "&id=524901";
-    https.get(url, function (resposnse) {
-        console.log(resposnse);
+
+    https.get(url, function (response) {
+        let data = "";
+
+        response.on("data", function (chunk) {
+            data += chunk;
+        });
+
+        response.on("end", function () {
+            try {
+                const weatherData = JSON.parse(data);
+
+                // Adjust this based on the structure of the response
+                const temp = weatherData.list[0].main.temp;
+
+                console.log(temp);
+            } catch (error) {
+                console.error("Error parsing JSON:", error);
+            }
+        });
     });
 
-
     res.send("Hey man!");
-})
-
-
+});
 
 app.listen(3030, function () {
     console.log("Server running at port 3030");
