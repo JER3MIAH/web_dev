8794a644d06045cd45c91918c61ba3af81ebbd43
JER3MIAH <jeremiahbrownie@gmail.com>
2023-11-21 20:00:52 +0100

    making http requests

--- weather_site/app.js
+++ weather_site/app.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const https = require("https");
+const apiKey = require("./constants")
 
 const app = express();
 
 
 
+app.get("/", function (req, res) {
 
+    const url = "https://api.openweathermap.org/data/2.5/forecast?q=london&appid=" + apiKey + "&id=524901";
+    https.get(url, function (resposnse) {
+        console.log(resposnse);
+    });
+
+
+    res.send("Hey man!");
+})
 
 
 
