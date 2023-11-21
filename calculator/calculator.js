e1b0f2246435244e0733242d0ac7a81a18aa866e
JER3MIAH <jeremiahbrownie@gmail.com>
2023-11-21 19:25:23 +0100

    update

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -9,8 +9,22 @@ app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/bmiCalculator", function (req, res) {
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmiCalculator", function (req, res) {
+    let height = parseFloat(req.body.height);
+    let weight = parseFloat(req.body.weight);
+    let bmi = weight / (height ** 2);
+    res.send("Your body mass index is " + bmi);
+});
+
 app.post("/", function (req, res) {
-    res.send("Thanks for the feedback.");
+    let num1 = Number(req.body.num1);
+    let num2 = Number(req.body.num2);
+    let result = num1 + num2;
+    res.send(num1 + " + " + num2 + " is " + result);
 });
 
 app.listen(3000, function () {
