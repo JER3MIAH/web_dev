aa15df637801d8f155f910262dfa6ae62f4f697c
JER3MIAH <jeremiahbrownie@gmail.com>
2023-11-22 13:03:42 +0100

    update

--- weather_site/app.js
+++ weather_site/app.js
@@ -1,41 +1,48 @@
 const apiKey = require("./constants");
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
-// const url = "https://api.openweathermap.org/data/2.5/forecast?q=london&appid=" + apiKey + "&id=524901&units=metric";
+app.post("/", function (req, res) {
+    const cityName = req.body.cityName;
+    const url = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey + "&id=524901&units=metric";
 
-// https.get(url, function (response) {
-//     let data = "";
+    https.get(url, function (response) {
+        let data = "";
 
-//     response.on("data", function (chunk) {
-//         data += chunk;
-//     });
+        response.on("data", function (chunk) {
+            data += chunk;
+        });
 
-//     response.on("end", function () {
-//         try {
-//             const weatherData = JSON.parse(data);
-//             const temp = weatherData.list[0].main.temp;
-//             const des = weatherData.list[0].weather[0].description;
-//             const weatherIcon = "https://openweathermap.org/img/w/" + weatherData.list[0].weather[0].icon + ".png";
-//             res.write("<h1>The temperature in london is " + temp + " degrees celcius</h1>");
-//             res.write("<p>The weather description is currently: " + des + " </p>");
-//             const imageDisplay = "<img src=" + weatherIcon + " alt=\"\">";
-//             res.write(imageDisplay);
-//             res.send();
+        response.on("end", function () {
+            try {
+                const weatherData = JSON.parse(data);
+                const temp = weatherData.list[0].main.temp;
+                const des = weatherData.list[0].weather[0].description;
+                const weatherIcon = "https://openweathermap.org/img/w/" + weatherData.list[0].weather[0].icon + ".png";
+                res.write("<h1>The temperature in " + cityName + " is " + temp + " degrees celcius</h1>");
+                res.write("<p>The weather description is currently: " + des + " </p>");
+                const imageDisplay = "<img src=" + weatherIcon + " alt=\"\">";
+                res.write(imageDisplay);
+                res.send();
 
-//             console.log(temp);
-//             console.log(des);
-//         } catch (error) {
-//             console.error("Error parsing JSON:", error);
-//         }
-//     });
-// });
+                console.log(temp);
+                console.log(des);
+            } catch (error) {
+                console.error("Error parsing JSON:", error);
+            }
+        });
+    });
+
+});
+
 
 app.listen(3030, function () {
     console.log("Server running at port 3030");
