016c467dac3f27a0a69e259d70e706428f1b3287
JER3MIAH <jeremiahbrownie@gmail.com>
2023-11-23 16:08:25 +0100

    update

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+// const path = require('path');
 
 
-const app = express(3000);
+const app = express();
+
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/signup.html");
