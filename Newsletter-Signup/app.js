aafdc4f98c09583b2948f03acb7b513b59f44a88
JER3MIAH <jeremiahbrownie@gmail.com>
2023-11-23 10:38:14 +0100

    starting a newletter sign up page

---
+++ Newsletter-Signup/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const app = express(3000);
+
+
+
+
+
+app.listen(3000, function () {
+    console.log("Server started at port 3000");
+});
