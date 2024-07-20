41517ed1e2aa1f101c1a46662bf7d46788fe3aeb
JER3MIAH <jeremiahbrownie@gmail.com>
2024-07-20 16:44:50 +0100

    QR Code Generator

--- QR Code Generator/index.js
+++ QR Code Generator/index.js
@@ -8,8 +8,6 @@ const url = await input({ message: 'Enter url' });
 var qr_svg = qr.image(url);
 qr_svg.pipe(fs.createWriteStream('qr_img.png'));
 
-// var svg_string = qr.imageSync('I love QR!');
-
 fs.writeFile('url.txt', url, function (err) {
     if (err) {
         throw err;
