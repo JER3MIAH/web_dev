7d1b20ea05583dee0c6fa2579bba25bf39f16d62
JER3MIAH <jeremiahbrownie@gmail.com>
2024-07-31 21:16:31 +0100

    Intro to react

---
+++ react/my-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import Fruit from "./Fruit";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Fruit />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
