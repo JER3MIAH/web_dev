8501b9156cd0315860cbede4f25794f5d3db170f
JER3MIAH <jeremiahbrownie@gmail.com>
2025-01-16 22:22:04 +0100

    _

--- react/card-component/src/App.jsx
+++
@@ -1,7 +0,0 @@
-import Card from "./card/Card";
-
-function App() {
-  return <Card />;
-}
-
-export default App;
--- react/my-react-app/src/App.jsx
+++
@@ -1,7 +0,0 @@
-import ColorPicker from "./components/ColorPicker/ColorPicker";
-
-function App() {
-  return <ColorPicker />;
-}
-
-export default App;
